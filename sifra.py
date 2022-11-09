bd726793046b601c149a7e5ee8ebc5791167a53b
EdMraz <eduard.mraz05@gmail.com>
2022-11-09 20:41:53 +0100

    fixed code

--- sifra.py
+++ sifra.py
@@ -2,9 +2,12 @@ def sifra(string:str,key:str)->str:
     y = 0
     nr = ""
     for i in string:
-        a = y % len(key)
-        x = ((ord(i)-97+ord(key[a])-97)%26)+97
-        y += 1
-        nr += chr(x)
+        if i != " ":
+            a = y % len(key)
+            x = ((ord(i)-97+ord(key[a])-97)%26)+97
+            y += 1
+            nr += chr(x)
+        else:
+            nr += " "
     return nr
-print(sifra("ahojsvet","kvet"))
+print(sifra("ahoj svet","kvet"))
--- sifra2decode.py
+++ sifra2decode.py
@@ -2,9 +2,12 @@ def sifra_decode(string:str,key:str)->str:
     y = 0
     nr = ""
     for i in string:
-        a = y % len(key)
-        x = ((ord(i)-97-ord(key[a])+97)%26)+97
-        y += 1
-        nr += chr(x)
+        if i != " ":
+            a = y % len(key)
+            x = ((ord(i)-97-ord(key[a])+97)%26)+97
+            y += 1
+            nr += chr(x)
+        else:
+            nr += " "
     return nr
 print(sifra_decode("kcsccqim","kvet"))
